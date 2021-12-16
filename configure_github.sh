537104a22c955512030983f7752aa4394043816c
Emiliano De Simoni <emiliano.desimoni@outlook.com>
2021-12-16 11:34:38 +0100

    Added comment to configure_github

--- configure_github.sh
+++ configure_github.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo
+echo "Based on https://github.com/cli/cli/blob/trunk/docs/install_linux.md guide"
+echo
 sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
 sudo dnf -y install gh
 gh auth login
